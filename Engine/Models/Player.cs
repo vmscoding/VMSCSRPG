37542ccca5e9514c88d8504466f571f5c62a155c
VMS <sarchale@gmail.com>
2024-08-03 18:43:35 -0500

    Added exp

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Player
+    {
+        public string Name {  get; set; }
+        public string Class { get; set; }
+        public int Hitpoints { get; set; }
+        public int Experience { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    internal class GameSession
+    {
+        Player CurrentPlayer {  get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Scott";
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.Experience = 10;
+        }
+    }
+}
